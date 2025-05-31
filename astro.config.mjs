abaad769c508501b6c5e118a4852845e931cd9d1
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2025-05-31 11:47:45 -0600

    feat: configuracion de proyecto agregada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: [
+                { find: '@', replacement: path.resolve(__dirname, './src') },
+                { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+                { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+                { find: '@layouts', replacement: path.resolve(__dirname, './src/layouts') },
+                { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+                { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+                { find: '@utils', replacement: path.resolve(__dirname, './src/utils') }
+            ]
+        }
+    }
+});
