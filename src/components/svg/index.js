d3b38b2fad9cae8a99f5bb0bd3d0c7d818a7d121
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2025-05-31 23:14:00 -0600

    feat: Iconos agregados

--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -1,4 +1,10 @@
 import LogoLolita from './LogoLolita.astro';
 import Nubes from './Nubes.astro';
+import IconoUbicacion from './IconoUbicacion.astro';
+import IconoPay from './IconoPay.astro';
+import IconoFlan from './IconoFlan.astro';
+import IconoCremita from './IconoCremita.astro';
+import IconoGelatina from './IconoGelatina.astro';
+import IconoCarlota from './IconoCarlota.astro';
 
-export { LogoLolita, Nubes };
+export { LogoLolita, Nubes, IconoUbicacion, IconoPay, IconoFlan, IconoCremita, IconoGelatina, IconoCarlota };
