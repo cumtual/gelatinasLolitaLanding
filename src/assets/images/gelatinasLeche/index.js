689b3d1537d7a2507a3f4259f773330135521074
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2025-06-10 00:32:30 -0600

    feat: imagenes agregadas

---
+++ src/assets/images/gelatinasLeche/index.js
@@ -0,0 +1,6 @@
+import GelatinaVainillaImage from './GelatinaVainilla.png';
+import GelatinaChocolateImage from './GelatinaChocolate.png';
+import GelatinaMelonImage from './GelatinaMelon.png';
+import GelatinaMosaico from './GelatinaMosaico.png';
+
+export { GelatinaVainillaImage, GelatinaChocolateImage, GelatinaMelonImage, GelatinaMosaico };
