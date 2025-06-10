e28173a82e62c7e44cf44a01ea6d3e06fcf3b214
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2025-06-10 00:30:23 -0600

    feat: configuraciones para render

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,25 @@ import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import react from '@astrojs/react';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        resolve: {
-            alias: [
-                { find: '@', replacement: path.resolve(__dirname, './src') },
-                { find: '@components', replacement: path.resolve(__dirname, './src/components') },
-                { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
-                { find: '@layouts', replacement: path.resolve(__dirname, './src/layouts') },
-                { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
-                { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
-                { find: '@utils', replacement: path.resolve(__dirname, './src/utils') }
-            ]
-        }
-    }
+  vite: {
+      resolve: {
+          alias: [
+              { find: '@', replacement: path.resolve(__dirname, './src') },
+              { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+              { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+              { find: '@layouts', replacement: path.resolve(__dirname, './src/layouts') },
+              { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+              { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+              { find: '@utils', replacement: path.resolve(__dirname, './src/utils') }
+          ]
+      }
+  },
+
+  integrations: [react()]
 });
